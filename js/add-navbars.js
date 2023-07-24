c8cab0d26db55f9ed64c12a4a1ea58e774286775
Artur Klesun <arturklesun@gmail.com>
2023-07-24 04:15:56 +0300

    Reuse header component on all header pages

--- js/add-navbars.js
+++ js/add-navbars.js
@@ -3,6 +3,11 @@ async function main() {
     const innerHTML = await fetch("/components/header.html").then(rs => rs.text());
     for (const header of document.getElementsByTagName("header")) {
         header.innerHTML = innerHTML;
+        for (const a of header.getElementsByTagName("a")) {
+            if (window.location.href === a.href) {
+                a.classList.add("current-page");
+            }
+        }
     }
 }
 
