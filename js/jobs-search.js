610a867d283e10c7a7d1d5ccb39611dddb45f365
Artur Klesun <arturklesun@gmail.com>
2023-07-27 12:25:14 +0300

    Add few new fields to jobs from RDB

--- js/jobs-search.js
+++ js/jobs-search.js
@@ -2,18 +2,25 @@
 const VACANCIES_ENDPOINT = "https://meliteh.azurewebsites.net/highlighted-vacancies";
 
 class Vacancy {
-    JobId = "523";
-    ClientId = "24546";
-    Location = "None";
-    JobTitle = "A340 Captain (1) - Legend Airlines";
-    StartDate = "2023-07-10 00:00:00";
-    StatusDate = "2023-07-19 14:07:27.223000";
-    CurrencyId = "12";
+    JobId = 476;
+    ClientId = "13592";
+    Company = "DAT LT";
+    Location = "Karmelava,Lithuania (LT / LTU)";
+    JobTitle = "ATR Engineer (1) - DAT";
+    StartDate = "2023-04-27 00:00:00";
+    StatusDate = "2023-04-27 14:53:49.957000";
+    CurrencyName = "EUR";
+    CurrencySymbol = "€";
     Category = "F2R Jobs";
-    PublishedJobDescription = "We are currently looking for experienced A340 Captain for the purpose of charter and cargo flight operations worldwide.\r\n\r\nQualification Requirements:\r\n- EASA Airline Transport Pilot’s License (ATPL)\r\n- Minimum 1500 hours on type (A340)\r\n- Minimum PIC 500 hours on type (A340)\r\n- Current on A340 or willing to endorse A340 in license by the time of joining (A320, A330 to A340 CCQ);\r\n- Valid EASA Class I Medical.\r\n- Minimum ICAO level 4 English proficiency\r\n- No Incidents, Accidents, Losses of Violations\r\n- Long- range flight experience, including Oceanic Area in advantage\r\n- Passed comprehensive background checks for drug and alcohol screening\r\n- Willing to relocate as per operational requirements during ACMI operations\r\n- Reference letters from previous employers.\r\n\r\nWe offer:\r\n- Great working conditions in young, friendly & professional team\r\n- Opportunity to grow in dynamically developing organization\r\n- Schedule planned according to EASA FTL requirements\r\n- Accommodation & travel costs covered by Legend Airlines during operations out of the base.";
-    CreatedOn = "2023-07-19 14:07:27.223000";
-    NoOfPlaces = "1";
-    ROWORDER = "1";
+    PublishedJobDescription = "First2 Resource behalf of DAT Airlines is looking for ATR MX.\r\n\r\nREQUIREMENTS:\r\n\r\n•\tValid Aircraft Engineer License, EASA Part 66 B1 or CAT A as minimum. B2 is also preferable.\r\n•\t6 months relevant experience in the last 2 consecutive years on ATR family aircrafts.\r\n•\tDeep knowledge of aircraft maintenance and operation.\r\n•\tValid certificates of HF, FTS and EWIS.\r\n•\tCitizenship of EU or other countries of Schengen area.\r\n•\tMust have a valid driver license & can pass the security clearance to access the airport\r\n\r\nPERSONAL QUANTITIES:\r\n\r\n•\tHigh sense of commitment and responsibility\r\n•\tAble to deal with various tasks in timely manner \r\n•\tVery good communication skills both oral and written English is essential \r\n•\tGood knowledge or ability to use software related to Aircraft Maintenance\r\n•\tAbility to learn\r\n\r\nWHAT WE OFFER:\r\n\r\n•\tCompetetive Fees\r\n•\tContract for 1 year with possibility to extend\r\n•\tWork Pattern: from 14/14 to 21/12 shift pattern or as agreed\r\n\r\nOnly successful candidates will be contacted.";
+    CreatedOn = "2023-04-27 14:48:50.263000";
+    NoOfPlaces = "6";
+    Description = "Contract";
+    Salary = 0;
+    MinBasic = 0;
+    MaxBasic = 0;
+    MinPackage = 0;
+    MaxPackage = 0;
 }
 
 async function main() {
@@ -23,17 +30,30 @@ async function main() {
     const template = document.getElementById("open-vacancy-card-template");
     const list = document.getElementsByClassName("open-vacancy-cards-list")[0];
     const cards = vacancies.map(vacancy => {
-        const card = template.content.cloneNode(true);
+        const card = template.content.firstElementChild.cloneNode(true);
         const [jobTitle, companyFromTitle] = vacancy.JobTitle.split("-");
-        card.querySelector(".company-name").textContent = companyFromTitle ?? "Company #" + vacancy.ClientId;
+        card.querySelector(".company-name").textContent = vacancy.Company;
         card.querySelector(".job-title-text").textContent = jobTitle.replace(/\(\d+\)\s*$/, "");
         card.querySelector(".vacancy-card-company-logo").style.backgroundImage = `url('img/company-logos/${vacancy.ClientId}.png')`;
         card.querySelector(".vacancy-description").textContent = (vacancy.PublishedJobDescription ?? "").split(/[\r\n]+/)[0];
         card.querySelector(".location-field").textContent = (vacancy.Location ?? "Globe").replace(/\(.*/, "").replace(/,.*/, "");
         card.querySelector(".time-posted-field").textContent = new Date(vacancy.CreatedOn).toISOString().slice(0, 10);
+        card.querySelector(".currency-symbol").textContent = vacancy.CurrencySymbol ?? vacancy.CurrencyName;
+        let salaryRangeStr;
+        if (vacancy.Salary) {
+            salaryRangeStr = vacancy.Salary
+        } else if (vacancy.MinBasic) {
+            salaryRangeStr = vacancy.MinBasic + "-" + vacancy.MaxBasic;
+        } else if (vacancy.MinPackage) {
+            salaryRangeStr = vacancy.MinPackage + "-" + vacancy.MaxPackage;
+        } else {
+            card.classList.add("no-salary-range");
+            salaryRangeStr = "";
+        }
+        card.querySelector(".salary-range").textContent = salaryRangeStr;
         return card;
     });
-    list.prepend(...cards)
+    list.prepend(...cards);
 }
 
 main().catch(error => {
