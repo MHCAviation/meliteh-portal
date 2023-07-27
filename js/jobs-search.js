46c6ac75833a7ca21020a9c64505acfc80886d4d
Artur Klesun <arturklesun@gmail.com>
2023-07-27 12:41:31 +0300

    Make job application link point to the real registration

--- js/jobs-search.js
+++ js/jobs-search.js
@@ -15,12 +15,13 @@ class Vacancy {
     PublishedJobDescription = "First2 Resource behalf of DAT Airlines is looking for ATR MX.\r\n\r\nREQUIREMENTS:\r\n\r\n•\tValid Aircraft Engineer License, EASA Part 66 B1 or CAT A as minimum. B2 is also preferable.\r\n•\t6 months relevant experience in the last 2 consecutive years on ATR family aircrafts.\r\n•\tDeep knowledge of aircraft maintenance and operation.\r\n•\tValid certificates of HF, FTS and EWIS.\r\n•\tCitizenship of EU or other countries of Schengen area.\r\n•\tMust have a valid driver license & can pass the security clearance to access the airport\r\n\r\nPERSONAL QUANTITIES:\r\n\r\n•\tHigh sense of commitment and responsibility\r\n•\tAble to deal with various tasks in timely manner \r\n•\tVery good communication skills both oral and written English is essential \r\n•\tGood knowledge or ability to use software related to Aircraft Maintenance\r\n•\tAbility to learn\r\n\r\nWHAT WE OFFER:\r\n\r\n•\tCompetetive Fees\r\n•\tContract for 1 year with possibility to extend\r\n•\tWork Pattern: from 14/14 to 21/12 shift pattern or as agreed\r\n\r\nOnly successful candidates will be contacted.";
     CreatedOn = "2023-04-27 14:48:50.263000";
     NoOfPlaces = "6";
+    /** Employment Type */
     Description = "Contract";
-    Salary = 0;
-    MinBasic = 0;
-    MaxBasic = 0;
-    MinPackage = 0;
-    MaxPackage = 0;
+    Salary = "0E-7";
+    MinBasic = "0E-7";
+    MaxBasic = "0E-7";
+    MinPackage = "0E-7";
+    MaxPackage = "0E-7";
 }
 
 async function main() {
@@ -32,19 +33,21 @@ async function main() {
     const cards = vacancies.map(vacancy => {
         const card = template.content.firstElementChild.cloneNode(true);
         const [jobTitle, companyFromTitle] = vacancy.JobTitle.split("-");
+        card.setAttribute("href", "https://1rww.eu/Secure/Login.aspx?JobId=" + vacancy.JobId);
         card.querySelector(".company-name").textContent = vacancy.Company;
         card.querySelector(".job-title-text").textContent = jobTitle.replace(/\(\d+\)\s*$/, "");
         card.querySelector(".vacancy-card-company-logo").style.backgroundImage = `url('img/company-logos/${vacancy.ClientId}.png')`;
         card.querySelector(".vacancy-description").textContent = (vacancy.PublishedJobDescription ?? "").split(/[\r\n]+/)[0];
         card.querySelector(".location-field").textContent = (vacancy.Location ?? "Globe").replace(/\(.*/, "").replace(/,.*/, "");
         card.querySelector(".time-posted-field").textContent = new Date(vacancy.CreatedOn).toISOString().slice(0, 10);
-        card.querySelector(".currency-symbol").textContent = vacancy.CurrencySymbol ?? vacancy.CurrencyName;
+        card.querySelector(".currency-symbol").textContent = vacancy.CurrencySymbol ?? vacancy.CurrencyName ?? "$";
+        card.querySelector(".employment-type").textContent = vacancy.Description;
         let salaryRangeStr;
-        if (vacancy.Salary) {
+        if (vacancy.Salary && vacancy.Salary !== "0E-7") {
             salaryRangeStr = vacancy.Salary
-        } else if (vacancy.MinBasic) {
+        } else if (vacancy.MinBasic && vacancy.MinBasic !== "0E-7") {
             salaryRangeStr = vacancy.MinBasic + "-" + vacancy.MaxBasic;
-        } else if (vacancy.MinPackage) {
+        } else if (vacancy.MinPackage && vacancy.MinPackage !== "0E-7") {
             salaryRangeStr = vacancy.MinPackage + "-" + vacancy.MaxPackage;
         } else {
             card.classList.add("no-salary-range");
