34b4f5c024605e48f5e6fa31864f8f442d6bb829
Artur Klesun <arturklesun@gmail.com>
2023-07-31 03:14:05 +0300

    Make contact form actually sned data

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,39 @@
 
+const SUBMIT_ENDPOINT = "https://meliteh-api.azurewebsites.net/submit-message";
+
 async function main() {
-    document.getElementById("message").focus();
+    document.querySelector("textarea[name=\"message\"]").focus();
 }
 
 main().catch(error => {
     console.error(error);
     alert("Failed to initialize page!");
 });
+
+/**
+ * @param {SubmitEvent} event
+ */
+window.submitMessage = async function(event) {
+    event.preventDefault();
+    /** @var {HTMLFormElement} */
+    const form = event.target;
+    const formData = new FormData(event.target);
+    form.setAttribute("disabled", "disabled");
+    form.setAttribute("data-status", "SUBMITTING");
+    form.querySelector(".status-message-content").textContent = "";
+    try {
+        const response = await fetch(SUBMIT_ENDPOINT, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(Object.fromEntries(formData)),
+        });
+        form.setAttribute("data-status", "SUCCESS");
+    } catch (error) {
+        form.removeAttribute("disabled");
+        form.setAttribute("data-status", "ERROR");
+        form.querySelector(".status-message-content").textContent = error?.message ?? String(error);
+    }
+    return false;
+};
