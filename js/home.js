a5c9cd555162110dfd2de2d634fdd00dadd842a3
Artur Klesun <arturklesun@gmail.com>
2023-08-02 11:25:46 +0300

    Make email subscribe form on the jobs search page work as well

--- js/home.js
+++ js/home.js
@@ -1,6 +1,4 @@
 
-const SUBSCRIBE_ENDPOINT = "https://meliteh-api.azurewebsites.net/subscribe-to-news";
-
 async function main() {
     const bg = document.getElementById("title-banner-slideshow-image");
     let iteration = 0;
@@ -9,37 +7,6 @@ async function main() {
         bg.setAttribute("data-slide-iteration", iteration % 4);
     }, 8000);
 }
-
-/**
- * @param {SubmitEvent} event
- */
-window.subscribeToNews = async function(event) {
-    event.preventDefault();
-    /** @var {HTMLFormElement} */
-    const form = event.target;
-    const formData = new FormData(form);
-    const input = form.elements["email"];
-    input.setAttribute("disabled", "disabled");
-    try {
-        const response = await fetch(SUBSCRIBE_ENDPOINT, {
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-            },
-            body: JSON.stringify({
-                email: formData.get("email"),
-            }),
-        });
-        if (response.status !== 200) {
-            throw new Error("unsuccessful server response: " + response.statusText);
-        }
-        // TODO: show as just a message
-        alert("Subscribed Successfully!");
-    } catch (error) {
-        input.removeAttribute("disabled", "disabled");
-        alert("Failed to subscribe: " + (error?.message ?? String(error)));
-    }
-};
 
 main().catch(error => {
     console.error(error);
---
+++ js/subscribe-to-news.js
@@ -0,0 +1,34 @@
+
+const SUBSCRIBE_ENDPOINT = "https://meliteh-api.azurewebsites.net/subscribe-to-news";
+
+/**
+ * @param {SubmitEvent} event
+ */
+window.subscribeToNews = async function(event) {
+    event.preventDefault();
+    /** @var {HTMLFormElement} */
+    const form = event.target;
+    const formData = new FormData(form);
+    const input = form.elements["email"];
+    const button = form.querySelector("button");
+    input.setAttribute("disabled", "disabled");
+    try {
+        const response = await fetch(SUBSCRIBE_ENDPOINT, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({
+                email: formData.get("email"),
+            }),
+        });
+        if (response.status !== 200) {
+            throw new Error("unsuccessful server response: " + response.statusText);
+        }
+        button.textContent = "Subscribed";
+        button.setAttribute("disabled", "disabled");
+    } catch (error) {
+        input.removeAttribute("disabled", "disabled");
+        alert("Failed to subscribe: " + (error?.message ?? String(error)));
+    }
+};
