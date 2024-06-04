624d42509ca692a382eca1588c74606b00f6201b
kris-mhc <140152714+kris-mhc@users.noreply.github.com>
2024-06-04 11:41:18 +0300

    adjust mobile and freeze background for pop up modal

--- js/popup-modal.js
+++ js/popup-modal.js
@@ -1,11 +1,13 @@
 function closePopup() {
-  document.getElementById('popup').style.display = 'none';
+  document.getElementById("popup").style.display = "none";
+  document.body.style.overflow = "visible";
 }
 
 function showPopup() {
-  document.getElementById('popup').style.display = 'flex';
+  document.getElementById("popup").style.display = "flex";
+  document.body.style.overflow = "hidden";
 }
 
-window.onload = function() {
+window.onload = function () {
   showPopup();
-}
+};
